467dbec96a0d62d4435ddea8624d5eda7b6bfed4
MWalden13 <mkwty8@umsystem.edu>
2020-09-10 15:25:05 -0500

    Created outline blueprint for code

--- mu-mips-v1/src/mu-mips.c
+++ mu-mips-v1/src/mu-mips.c
@@ -307,6 +307,26 @@ void handle_instruction()
 {
 	/*IMPLEMENT THIS*/
 	/* execute one instruction at a time. Use/update CURRENT_STATE and and NEXT_STATE, as necessary.*/
+
+	/*
+	CODE OUTLINE
+
+	1. Load instruction using mem_read_32 function
+
+	2. Increment instruction to separate op, rs, rt, rd, shamt, funct, address, immediate
+
+	3. Use switch-case function with opcode to determine which instruction we have
+
+	4. Perform operations to correctly execute instruction
+
+	5. Update Current/Next State
+
+
+	*/
+
+	
+	
+
 }
 
 
@@ -339,6 +359,19 @@ void print_program(){
 /************************************************************/
 void print_instruction(uint32_t addr){
 	/*IMPLEMENT THIS*/
+
+	/*
+
+	1. Same as HandleInstruction
+
+	2. Same as HandleInstruction
+
+	3. Same as HandleInstruction
+
+	4. Instead of performing operation, simply print the instructions inputs/outputs
+
+	*/
+
 }
 
 /***************************************************************/
